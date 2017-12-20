08197b3a9cf2503f22ebb1f57dc5d635c2f2c4ca
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2017-12-20 14:24:07 -0700

    wrote a prototype that shows the client to choose a song

--- View.js
+++ View.js
@@ -6,6 +6,10 @@
     return event.target.children[event.target.selectedIndex].innerText + " has been selected!";
   }
 
+  View.prototype.chooseASong = function() {
+    return "Choose a song please!";
+  }
+
   global.View = View;
 
 })(this);
