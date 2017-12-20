eb295c83fad5a7952254dc7b67c75771804346a2
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2017-12-20 14:24:51 -0700

    took the play and pause outside the selection event, since it doesn't need to share the same event

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ var pauseBtn = document.querySelector('#pause-btn');
 
 selection.addEventListener('change', function(event) {
   songResults.innerHTML = jukebox.selectedSong(event, view);
-  playBtn.addEventListener('click', function() {
-    jukebox.playSong();
-  });
-  pauseBtn.addEventListener('click', function() {
-    jukebox.pauseSong();
-  });
+});
+
+playBtn.addEventListener('click', function() {
+  jukebox.playSong();
+});
+
+pauseBtn.addEventListener('click', function() {
+  jukebox.pauseSong();
 });
